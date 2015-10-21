0096af7235846c279b59c9f0dbb248bc7a62593e
lgrube <lgv9483@saintjoe.edu>
2015-10-21 08:42:19 -0500

    think i have express entered in correctly

--- ch07/7.2.8.1-express-static/app.js
+++ ch07/7.2.8.1-express-static/app.js
@@ -13,42 +13,78 @@
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
-  http    = require( 'http'    ),
-  express = require( 'express' ),
+  http    = require('http'),
+  express = require('express'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler'),
 
   app     = express(),
   server  = http.createServer( app );
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure( function () {
-  app.use( express.bodyParser() );
-  app.use( express.methodOverride() );
-  app.use( express.static( __dirname + '/public' ) );
-  app.use( app.router );
+app.use(bodyParser.urlencoded());
+app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('common'));
+app.use(errorHandler());
+
+//  app.use( errorHandler({
+//  dumpExceptions : true,
+//    showStack      : true
+// });
+// all configurations below are for routes
+app.get('/', function(request, response) {
+  response.redirect('/spa.html');
 });
 
-app.configure( 'development', function () {
-  app.use( express.logger() );
-  app.use( express.errorHandler({
-    dumpExceptions : true,
-    showStack      : true
-  }) );
+app.all('/user/*?', function(request, response, next) {
+  response.contentType('json');
+  next();
 });
 
-app.configure( 'production', function () {
-  app.use( express.errorHandler() );
+app.get('/user/list', function(request, response) {
+  response.send({ title: 'user list' });
 });
 
-app.get( '/', function ( request, response ) {
-  response.redirect( '/spa.html' );
+app.post('/user/create', function(request, response) {
+  response.send({ title: 'user created' });
 });
+
+app.get('/user/read/:id([0-9]+)',
+  function(request, response) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' found'
+    });
+  }
+);
+
+app.post('/user/update/:id([0-9]+)',
+  function(request, response) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' updated'
+    });
+  }
+);
+
+app.get('/user/delete/:id([0-9]+)',
+  function(request, response) {
+    response.send({
+      title: 'user with id ' + request.params.id + ' deleted'
+    });
+  }
+);
+
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 3000 );
+server.listen(3000);
 console.log(
   'Express server listening on port %d in %s mode',
    server.address().port, app.settings.env
 );
+
 // ------------------ END START SERVER --------------------
