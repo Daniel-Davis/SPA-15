8a5cb80afb81049d226090210512b0644e57cfea
lgrube <lgv9483@saintjoe.edu>
2015-10-23 15:24:24 -0500

    all of chapter 8 with mongodb and node working correctly

--- ch08/07-base/routes.js
+++ ch08/07-base/routes.js
@@ -12,17 +12,7 @@
 
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
-var configRoutes,
-mongodb   = require('mongodb'),
-mongoServer = new mongodb.Server(
-  'localhost',
-  mongodb.Connection.DEFAULT_PORT
-),
-dbHandle    = new mongodb.Db(
-'spa', mongoServer, { safe : true } );
-dbHandle.open( function () {
-  console.log( '** Connected to MongoDB **' );
-});
+var configRoutes;
 // ------------- END MODULE SCOPE VARIABLES ---------------
 
 // ---------------- BEGIN PUBLIC METHODS ------------------
